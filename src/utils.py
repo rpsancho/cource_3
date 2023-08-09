070e1663cd4ae47d483ec975472c08953c60aabc
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-08-10 00:15:39 +0700

    add docstring to all functions

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,22 @@ import json
 
 
 def get_list_from_json(file_path):
+    """
+    read json file and convert into python data structure
+    :param file_path: path to json file
+    :return: data
+    """
     with open(file_path, 'r', encoding='UTF-8') as fp:
         data = json.load(fp)
     return data
 
 
 def remove_invalid_data_from_list(src_list):
+    """
+    remove invalid items from src_list and return valid_list
+    :param src_list: source list
+    :return: valid_list
+    """
     valid_list = []
     for item in src_list:
         if not item:
@@ -19,10 +29,24 @@ def remove_invalid_data_from_list(src_list):
 
 
 def sort_operations_by_date(src_list):
+    """
+    sort items in src_list by date from newest to oldest
+    and return new sorted list
+    :param src_list: source list
+    :return: sorted list
+    """
     return sorted(src_list, key=lambda x: x['date'], reverse=True)
 
 
 def get_latest_operations(src_list, quantity=10, state='ANY'):
+    """
+
+    :param src_list: source list
+    :param quantity: quantity of latest operations
+    :param state: get values 'ANY' (default), 'EXECUTED', 'CANCELED'
+    filter results by state
+    :return: filtered list of operations
+    """
     if state == 'ANY':
         return src_list[0:quantity]
 
@@ -39,10 +63,20 @@ def get_latest_operations(src_list, quantity=10, state='ANY'):
 
 
 def format_date(date_str):
+    """
+    convert date string to YYYY.MM.DD format
+    :param date_str: source date string
+    :return: formatted date string
+    """
     return date_str[8:10]+'.'+date_str[5:7]+'.'+date_str[0:4]
 
 
 def format_requisites(req_str):
+    """
+    hide digits in card numbers or bank accounts
+    :param req_str: source string with requisites
+    :return: formatted string with hidden requisites
+    """
     req_list = req_str.split(' ')
     number = req_list[-1]
     if req_list[0] == "Счет":
